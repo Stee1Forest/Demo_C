fc8f57a5058f3cffd7a0491ff19f1de490f72a27
Stee1Forest <1184401036@qq.com>
2023-08-31 20:07:24 +0200

    constructor

--- C/Day_03/Constructor/Classification_of_Constructor.cpp
+++ C/Day_03/Constructor/Classification_of_Constructor.cpp
@@ -22,10 +22,22 @@ public:
         cout << "有参数构造函数" << endl;
     }
 
+    //有参构造函数重载
+    Constructor(int a)
+    {
+        m_a = a;
+        m_b = 0;
+        cout << "有参数构造函数重载" << endl;
+    }
+
     //赋值/拷贝构造函数
     Constructor(const Constructor & obj)
     {
         cout << "拷贝构造函数" << endl;
+    }
+    void printConstructor()
+    {
+        cout << "普通成员函数" << endl;
     }
 protected:
 
@@ -37,6 +49,9 @@ private:
 int main()
 {
     Constructor t1;
+    //Constructor t1();         调用无参构造函数不能有();
+    Constructor t2 = (3, 4);    //C++对=进行了功能增强（运算符重载）
+    t1.printConstructor();
     system("pause");
     return 0;
 }
