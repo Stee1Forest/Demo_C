db322b64e0375841d6009633db9a327eb70e2fd9
Stee1Forest <1184401036@qq.com>
2023-08-31 20:19:53 +0200

    constructor

--- C/Day_03/Constructor/Classification_of_Constructor.cpp
+++ C/Day_03/Constructor/Classification_of_Constructor.cpp
@@ -50,8 +50,14 @@ int main()
 {
     Constructor t1;
     //Constructor t1();         调用无参构造函数不能有();
-    Constructor t2 = (3, 4);    //C++对=进行了功能增强（运算符重载）
+    Constructor t2 = (3, 4);    //C++对=进行了功能增强（运算符重载），(3,4)为逗号表达式，逗号表达式为最后一个的值
+    //直接调用构造函数完成对对象的初始化
+    Constructor t3 = Constructor(3, 4);     //直接调用构造函数会C++编译器会创造一个匿名对象，其声明周期取决于如何去使用，只会调用一次构造函数
+    //对象的初始化和对象的赋值是两个不同的概念，Constructor t3 = Constructor(3, 4);为对象初始化
+    t1 = t3;
+    
     t1.printConstructor();
+    t2.printConstructor();
     system("pause");
     return 0;
 }
